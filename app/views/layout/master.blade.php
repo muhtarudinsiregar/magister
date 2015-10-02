91615682e839e6f10b65e634ce04bfc2e7e66ed1
muhtarudinsiregar <redcar.studious@gmail.com>
2015-10-02 10:20:56 +0700

    fix no hand pointer + add img in master

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -13,6 +13,20 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
+				<nav class="navbar navbar-default navbar-img" role="navigation">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand brand-custom" href=""><img src="{{ asset('images/logofulls2.png') }}" alt=""></a>
+					</div>
+				</nav>
+			</div>
+			<div class="col-lg-12">
 				<nav class="navbar navbar-default" role="navigation">
 
 					<!-- Brand and toggle get grouped for better mobile display -->
@@ -23,21 +37,20 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="{{url('/')}}">Magister PPs UII</a>
 					</div>
-
+					<a class="navbar-brand">{{-- <p class="p-custom">Magister</p> --}}</a>
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<li><a onclick="return false;" href="{{ url ('beranda')}}">Beranda</a></li>
-							<li><a onclick="return false;" href="{{ url ('programstudi')}}">Program Studi</a></li>
-							<li><a onclick="return false;" href="{{ url('data-pribadi')}}">Data Pribadi</a></li>
-							<li><a onclick="return false;" href="{{ url('pendidikan')}}">Pendidikan</a></li>
-							<li><a onclick="return false;" href="{{ url('pekerjaan')}}">Pekerjaan</a></li>
-							<li><a onclick="return false;" href="{{ url('pendanaan')}}">Pendanaan</a></li>
-							<li><a onclick="return false;" href="{{ url('kontak')}}">Kontak</a></li>
-							<li><a onclick="return false;" href="{{ url('jadwal')}}">Jadwal Tes</a></li>
-							<li><a onclick="return false;" href="{{ url('pernyataan')}}">Pernyataan</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url ('beranda')}}">Beranda</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url ('programstudi')}}">Program Studi</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('data-pribadi')}}">Data Pribadi</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('pendidikan')}}">Pendidikan</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('pekerjaan')}}">Pekerjaan</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('pendanaan')}}">Pendanaan</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('kontak')}}">Kontak</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('jadwal')}}">Jadwal Tes</a></li>
+							<li><a class="no-hand" onClick="return false" href="{{ url('pernyataan')}}">Pernyataan</a></li>
 						</ul>
 					</div><!-- /.navbar-collapse -->
 				</nav>
